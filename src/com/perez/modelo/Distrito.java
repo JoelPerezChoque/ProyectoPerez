4702a21f2f79c9676abbb8d13fdaa1e3444cac2a
Joel Perez Choque <hernan3506@gmail.com>
2014-06-25 14:33:05 -0500

    Update Distrito.java

--- src/com/perez/modelo/Distrito.java
+++ src/com/perez/modelo/Distrito.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package com.perez.modelo;
 
-/**
- *
- * @author alum.fial7
- */
+
 public class Distrito {
     private int idd;
     private int idr;
